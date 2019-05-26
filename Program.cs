c4f8a90b4d591b09f118a18231fb176f4212e359
Christopher Jenkins <chris.jenkins20102012@gmail.com>
2019-05-26 00:15:35 -0600

    got transliteration option file done.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace transliterator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var line = "";
+            while ((line = Console.ReadLine()) != null && line != "") {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Goodbye World!");              
         }
     }
 }
